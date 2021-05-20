a08211df68d222ddafa66183f292695ffdd431de
Blaire <grant.blaire@gmail.com>
2021-05-19 21:23:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    changeElement();
+  });
+  
+function changeElement() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+};
